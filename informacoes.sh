b67e6074ac955c502fd48d25661a89a428421f45
Clayton Pereira <claytonp_13@hotmail.com>
2023-07-27 10:40:04 -0300

    Update informacoes.sh

--- informacoes.sh
+++ informacoes.sh
@@ -1,36 +1,32 @@
-#!/bin/bash 
+#!/usr/bin/env bash 
 #### Script para coletar informacoes da maquina
 
 # Autor: Clayton Pereira 
-# Contato:    Email      claytonp_13@hotmail.com 
-#             Telegram   @Claytonpx13 ou https://telegram.me/Claytonpx13
-# Data da criacao 28/10/2016
+# Contato:    Email:      claytonp_13@hotmail.com 
+#             Telegram:   @Clayton_Pereira ou https://telegram.me/Clayton_Pereira
+# Data da criacao:        28/10/2016
+# Ultima atualizacao:     27/07/2023
 
 HARDWARE(){
 	# Coleta info de memoria
-	MEM=$(cat /proc/meminfo | grep MemTotal: | sed 's/MemTotal:       //' | sed 's/ kB//' | sed 's/.*/echo "& \/ 1024" | bc/' | bash | sed 's/.*/& MB/')
-	MEM="$(echo ${MEM})"
+	MEM=$(grep "MemTotal:" /proc/meminfo | sed 's/MemTotal:       //' | sed 's/ kB//' | sed 's/.*/echo "& \/ 1024" | bc/' | bash | sed 's/.*/& MB/')
 
 	# Coleta info processador - quantidade
-	NPROC="$(cat /proc/cpuinfo | grep -i processor | wc -l)"
-	NPROC="$(echo ${NPROC})"
+	NPROC=$(grep -c "processor" /proc/cpuinfo)
 
 	# Coleta info processador - modelo
-	PROC="$(cat /proc/cpuinfo | grep "\model name" | tail -1 | cut -d\: -f2-)"
-	PROC="$(echo ${PROC})"
+	PROC=$(grep "\model name" /proc/cpuinfo | tail -1 | cut -d':' -f2-)
 
 	# Coleta info discos
-	DISK=$(fdisk -l | grep Disk | egrep -v "identifier" | cut -d ' ' -f2-4 | cut -d ',' -f1)
+	DISK=
 	echo
 	echo -e '\033[32;1m ==== Informacoes hardware ==== \033[m'
 	
-cat << EOT
-Memoria ............: ${MEM}
-Memoria Swap .......: $(cat /proc/meminfo | grep SwapTotal | awk '{print $2}' | sed 's/.*/& KB/')
-Processador ........: [ ${NPROC} ] ${PROC}
-Disco(s) ...........: $DISK
-EOT
-
+	echo " Memoria ............: ${MEM}
+ Memoria Swap .......: $(grep SwapTotal /proc/meminfo | awk '{print $2}' | sed 's/.*/& KB/')
+ Processador ........: [ ${NPROC} ] ${PROC}
+ Disco(s) ...........: $DISK
+"
 }
 
 REDE(){
@@ -46,113 +42,97 @@ REDE(){
 	NIC=$(ip addr list | grep BROADCAST | awk -F ':' '{print $2}'| tr '\n' ' ')
 
 	# Gateway
-	for i in $NIC;do
-		IP=$(ifconfig $i | egrep -o "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" | tail -3 | head -1)
-		BCAST=$(ifconfig $i | egrep -o "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" | tail -2 | head -1)
-		MASK=$(ifconfig $i | egrep -o "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" | tail -1 | head -1)
-		REDE=$(ip ro | egrep "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\/[1-3]{1,2}.*$i.*$IP" | egrep -o "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\/[0-9]{1,2}")
+	for i in $NIC; do
+		IP=$(ifconfig "$i" | grep -E -o "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" | tail -3 | head -1)
+		BCAST=$(ifconfig "$i" | grep -E -o "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" | tail -2 | head -1)
+		MASK=$(ifconfig "$i" | grep -E -o "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" | tail -1 | head -1)
+		REDE=$(ip ro | grep -E "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\/[1-3]{1,2}.*$i.*$IP" | grep -E -o "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\/[0-9]{1,2}")
 		MAC_ADDR=$(ifconfig | grep HWaddr | cut -d' ' -f9)
-		ip ro | grep -o "default equalize" > /dev/null
 
-		if [ $? -eq 0 ];then
-			GW=$(ip ro | egrep  ".*nexthop.*$i" | egrep -o "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}")
+		if ip ro | grep -o "default equalize" > /dev/null; then
+			GW=$(ip ro | grep -E ".*nexthop.*$i" | grep -E -o "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}")
 		else
-			GW=$(ip ro | egrep  ".*default.*$i" | egrep -o "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}")   
+			GW=$(ip ro | grep -E ".*default.*$i" | grep -E -o "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}")   
 		fi
 
-		if [ $IP != $IP ];then
-			echo "Interface: $i"
-			echo "Endereco IP .......: -----"
-			echo
-		else
-			echo "Interface .........: $i"
-			echo "Endereco IP .......: $IP"
-			echo "Endereco Fisico ...: $MAC_ADDR"
-			echo "Broadcast .........: $BCAST"
-			echo "Mascara:...........: $MASK"
-			echo "Rede ..............: $REDE"
-			echo "Gateway ...........: $GW"
-			echo
-		fi
+		echo " Interface .........: $i
+ Endereco IP .......: $IP
+ Endereco Fisico ...: $MAC_ADDR
+ Mascara ...........: $BCAST
+ Broadcast .........: $MASK
+ Rede ..............: $REDE
+ Gateway ...........: $GW
+"
 	done
 
 	DNS=$(awk '/nameserver/ {print $2}' /etc/resolv.conf | tr -s '\n' ' ')
-	echo -e "DNS Servers........: $DNS"
-	echo
+	echo " DNS Servers........: $DNS
+"
 }
 
 KERNEL(){
-	echo -e '\033[32;1m ==== Informacoes do Kernel ==== \033[m'
-	echo "Kernel-name .......: $(uname -s -o)"
-	echo "Kernel-release ....: $(uname -r)"
-	echo "Kernel-version ....: $(uname -v)"
-	echo
+	echo -e "\033[32;1m ==== Informacoes do Kernel ==== \033[m
+ Kernel-name .......: $(uname -s -o)
+ Kernel-release ....: $(uname -r)
+ Kernel-version ....: $(uname -v)
+"
 }
 
 SISTEMA(){
 	echo -e '\033[32;1m ==== Informacoes do Sistema ==== \033[m'
-	DISTRIB_DESCRIPTION=$(cat /etc/*-release | grep PRETTY_NAME | cut -d'"' -f2)
-	HOME_URL=$(cat /etc/*-release | grep HOME_URL | cut -d'"' -f2)
-	SUPPORT_URL=$(cat /etc/*-release | grep SUPPORT_URL | cut -d'"' -f2)
+	DISTRIB_DESCRIPTION=$(grep PRETTY_NAME /etc/*-release | cut -d'"' -f2)
+	HOME_URL=$(grep HOME_URL /etc/*-release | cut -d'"' -f2)
+	SUPPORT_URL=$(grep SUPPORT_URL /etc/*-release | cut -d'"' -f2)
 
-	echo "Nome da maquina ...: $(hostname)"
-	echo "Nome do SO ........: $DISTRIB_DESCRIPTION"
-	echo "Site Oficial ......: $HOME_URL"
-	echo "URL para suporte ..: $SUPPORT_URL"
-	echo
-}
-
-SERVICOS(){
-	echo -e '\033[32;1m ==== Informacoes do(s) Servico(s) ==== \033[m'
-	echo "Ainda em construcao..."
-	echo
+	echo " Nome da maquina ...: $(hostname)
+ Nome do SO ........: $DISTRIB_DESCRIPTION
+ Site Oficial ......: $HOME_URL
+ URL para suporte ..: $SUPPORT_URL
+"
 }
 
 AJUDA(){
-	echo -e '\033[32;1m  ==== Tela de Ajuda ==== \033[m'
-	echo " Este script foi criado para colotar e imprimir na tela"
-	echo "as informacoes mais importantes do sistema operacional como"
-	echo "tambem informacoes de hardware."
-	echo
-	echo " Este script nao funciona sem um parametro!"
-	echo "USE: $(basename $0) <<PARAMETRO>>"
-	echo
-	echo -e '\033[32;1m PARAMETROS \033[m'
-	echo -e "  \033[37;1m-a\033[m ou \033[37;1m--all\033[m"
-	echo "    Para exibir todas as informacoes, os dados seram mostradas na seguinte ordem:"
-	echo "      Campos Hardware... Sistema... Kernel... Rede... Servicos."
-	echo
-	echo -e "  \033[37;1m-H\033[m ou \033[37;1m--hardware\033[m"
-	echo "    Exibe informacoes do seu Hardware."
-	echo "      Quantidade de memoria RAM (MB)"
-	echo "      Quantidade de memoria SWAP (MB)"
-	echo "      Processador, quantidade de nucleos e modelo"
-	echo "      Disco(s) caminho e tamanho do(s) disco(s)"
-	echo
-	echo -e "  \033[37;1m-h\033[m ou \033[37;1m--help\033[m"
-	echo "    Mostra esta tela de ajuda."
-	echo
-	echo -e "  \033[37;1m-k\033[m ou \033[37;1m--kernel\033[m"
-	echo "    Informacoes do kernel"
-	echo "      name, realease e version."
-	echo
-	echo -e "  \033[37;1m-r\033[m ou \033[37;1m--rede\033[m"
-	echo "     Informacoes de rede"
-	echo "       Endereco de rede"
-	echo "       IP address e mascara de rede"
-	echo "       Gateway padrao e dns"
-	echo "       Broadcast e endereco fisico (MAC ADDRESS)"
-	echo
-	echo -e "  \033[37;1m-S\033[m ou \033[37;1m--services\033[m"
-	echo
-	echo -e "  \033[37;1m-s\033[m ou \033[37;1m--system\033[m"
+	echo -e "\033[32;1m  ==== Tela de Ajuda ==== \033[m
+  Este script foi criado para colotar e imprimir na tela
+ as informacoes mais importantes do sistema operacional como
+ tambem informacoes de hardware.
+
+  Este script nao funciona sem um parametro!
+ USE: $(basename "$0") <<PARAMETRO>>
+
+\033[32;1m PARAMETROS \033[m
+  \033[37;1m-a\033[m ou \033[37;1m--all\033[m
+    Para exibir todas as informacoes, os dados seram mostradas na seguinte ordem:
+      Campos Hardware... Sistema... Kernel... Rede... Servicos.
+
+  \033[37;1m-H\033[m ou \033[37;1m--hardware\033[m
+    Exibe informacoes do seu Hardware.
+      Quantidade de memoria RAM (MB)
+      Quantidade de memoria SWAP (MB)
+      Processador, quantidade de nucleos e modelo
+      Disco(s) caminho e tamanho do(s) disco(s)
+
+  \033[37;1m-h\033[m ou \033[37;1m--help\033[m
+    Mostra esta tela de ajuda.
+
+  \033[37;1m-k\033[m ou \033[37;1m--kernel\033[m
+    Informacoes do kernel
+      name, realease e version.
+
+  \033[37;1m-r\033[m ou \033[37;1m--rede\033[m
+     Informacoes de rede
+       Endereco de rede
+       IP address e mascara de rede
+       Gateway padrao e dns
+       Broadcast e endereco fisico (MAC ADDRESS)
+
+  \033[37;1m-s\033[m ou \033[37;1m--system\033[m"
 }
 
-if [ $(id -u) != "0" ];then
+if [[ $(id -u) != "0" ]];then
 	echo
-	echo -e "O usuário atual não é o ROOT, execute o comando [sudo -i]<ENTER> depois digite sua senha"
-	echo -e "Pressione <ENTER> para finalizar o script"
-	read
+	echo -e " O usuário atual não é o ROOT, execute o comando [sudo -i]<ENTER> depois digite sua senha"
+
 	exit 1
 fi
 
@@ -173,22 +153,15 @@ case $1 in
 		SISTEMA
 	;;
 
-	-S | --services)
-		SERVICOS
-	;;
-
 	-a | --all)
 		HARDWARE
 		SISTEMA
 		KERNEL
 		REDE
-		SERVICOS
 	;;
 
 	-h | --help)
-		#AJUDA
-		echo "  Lamento..."
-		echo " Parametro ainda em construcao :("
+		AJUDA
 	;;
 
 	*)
@@ -198,6 +171,3 @@ case $1 in
 		exit 1
 	;;
 esac
-
-echo
-exit 0
